8a758b730ed5d17e790f014f84db519e7d4dcdb0
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-05-06 00:15:00 +0800

    feat(frontend): Initialized Svelte-CapacitorJs boilerplate

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ioteamnida.socketsens',
+  appName: 'SocketSens',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
