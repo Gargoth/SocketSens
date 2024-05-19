244c0267895903c86622e69de4ada990d5bbeaac
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2024-05-19 22:57:50 +0800

    feat: added arduino code as separate branch

---
+++ PZEM004t/PZEM004t.ino
@@ -0,0 +1,85 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+Example of using one PZEM module with Software Serial interface.
+================================================================
+
+If only RX and TX pins are passed to the constructor, software 
+serial interface will be used for communication with the module.
+
+*/
+
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+
+#if defined(ESP32)
+    #error "Software Serial is not supported on the ESP32"
+#endif
+
+/* Use software serial for the PZEM
+ * Pin 12 Rx (Connects to the Tx pin on the PZEM)
+ * Pin 13 Tx (Connects to the Rx pin on the PZEM)
+*/
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 12
+#define PZEM_TX_PIN 13
+#define RELAY_OUTPUT 5
+#endif
+
+
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+void setup() {
+    /* Debugging serial */
+    Serial.begin(115200);
+    pinMode(RELAY_OUTPUT, OUTPUT);    // sets the digital pin 13 as output
+}
+
+void loop() {
+         
+    Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
+        Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+    }
+
+    Serial.println();
+    delay(1000);
+
+    digitalWrite(RELAY_OUTPUT, HIGH); // sets the digital pin 13 on
+    Serial.println("LED ON.");  
+    delay(500);            // waits for a second
+    digitalWrite(RELAY_OUTPUT, LOW);  // sets the digital pin 13 off
+    Serial.println("LED OFF.");
+    delay(500);            // waits for a second
+}
--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-vercel';
-
-export default {
-	kit: {
-		adapter: adapter({
-			// see below for options that can be set here
-		})
-	}
-};
