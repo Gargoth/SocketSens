85dd6ea9d4c146ffa9a914f1909587871f5e3915
francecalma <113011006+francecalma@users.noreply.github.com>
2024-05-24 09:43:59 +0800

    feat: display users table in notifications page

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,4 +1,10 @@
 import { json } from '@sveltejs/kit';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://dewnlawwegxhuprfzbrn.supabase.co';
+const supabaseAnonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRld25sYXd3ZWd4aHVwcmZ6YnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NjU5NDYsImV4cCI6MjAzMTQ0MTk0Nn0.OIPl5-jEv0qsIG_-pKqMOvKNVkT_wUyRGWbgTp1nzw8';
+
+const supabase = createClient(supabaseUrl, supabaseAnonkey);
 
 export async function GET(event) {
   const sampleValue = {
