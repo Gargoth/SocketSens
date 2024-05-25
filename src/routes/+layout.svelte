72751b836e2ff2f2bbc7bb65ec814b0e3e3ca5ad
splasherzz <juliasabado30@gmail.com>
2024-05-25 22:28:29 +0800

    style: add logo on landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,41 @@
     import Menu from 'virtual:icons/mdi/menu';
     import { page } from '$app/stores';
 
+    import blackLogo from '$lib/logo-black.png'
+    import whiteLogo from '$lib/logo-white.png'
+
     let showNavbar = false;
     let userId = "89";
 
+    //$: console.log($page.url.pathname, showNavbar);
     $: navclass = showNavbar ? 'navbar' : '';
-    $: console.log($page.url.pathname, showNavbar);
+    $: logoSrc = $page.url.pathname === "/" ? whiteLogo : blackLogo
 
     function toggleNavbar() {
         showNavbar = !showNavbar;
     }
 </script>
 
-<nav class={"p-10 flex flex-row-reverse items-end " + ($page.url.pathname !== '/' ? 'bg-white' : '')}>
+<nav class={"pl-6 pr-10 pt-10 flex flex-row-reverse items-center justify-between items-end " + ($page.url.pathname !== '/' ? 'bg-white' : '')}>
     <button on:click={toggleNavbar} class="z-50 relative">
         <Menu style={"font-size: 1.7em; " + ($page.url.pathname === "/" || showNavbar ? "color: white;" : "color: black;")} />
     </button>
+    <div class="flex items-center">
+        {#if $page.url.pathname === "/"}
+            <img src={logoSrc} alt="logo" width="50px"/>
+        {/if}
+        {#if $page.url.pathname === "/"}
+            <div class="title ml-1.5">
+                SocketSens
+            </div>
+        {/if}
+    </div>
+    {#if $page.url.pathname !== "/"}
+        <div></div>
+    {/if}
 </nav>
 
-<div class={`fixed overflow-hidden top-0 right-0 h-screen w-screen bg-[#DF793F] z-40 text-white flex flex-col gap-y-3 px-9 py-28 transition-transform duration-300 ease-in-out ${showNavbar ? 'translate-x-0' : 'translate-x-full'}`}>
+<div class={`fixed overflow-hidden top-0 right-0 overflow-hidden h-screen w-screen bg-[#DF793F] z-40 text-white flex flex-col gap-y-3 px-9 py-28 transition-transform duration-300 ease-in-out ${showNavbar ? 'translate-x-0' : 'translate-x-full'}`}>
     {#if userId === ""}
         <a href="/" on:click={toggleNavbar}>Home</a>
         <a href="/login" on:click={toggleNavbar}>Login</a>
@@ -31,7 +48,9 @@
     {/if}
 </div>
 
-<slot />
+<div class="h-9/12">
+    <slot />
+</div>
 
 <style>
     @font-face {
@@ -47,6 +66,12 @@
     @font-face {
         font-family: "EncodeMed";
         src: url("/fonts/EncodeSansExpanded-Medium.ttf") format("truetype");
+    }
+
+    .title {
+        font-family: "EncodeBold", sans-serif;
+        font-size: 1.4em;
+        color: white;
     }
 
     .translate-x-full {
