482999494f378a55021e19004c871de1c0d2c267
splasherzz <juliasabado30@gmail.com>
2024-05-25 21:32:12 +0800

    style: add navbar animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,37 @@
 <script>
     import "../app.css";
-    import Menu from 'virtual:icons/mdi/menu'
-    import { getStores, navigating, page, updated } from '$app/stores';
+    import Menu from 'virtual:icons/mdi/menu';
+    import { page } from '$app/stores';
 
     let showNavbar = false;
     let userId = "89";
 
-    $: navclass = showNavbar ? 'navbar' : ''
-    $: console.log($page.url.pathname, showNavbar) 
+    $: navclass = showNavbar ? 'navbar' : '';
+    $: console.log($page.url.pathname, showNavbar);
 
     function toggleNavbar() {
         showNavbar = !showNavbar;
     }
-
 </script>
 
-<nav class={navclass + " p-10 flex flex-row-reverse items-end " + ($page.url.pathname === "/" ? "" : "bg-white")}>
-    <button on:click={() => toggleNavbar()}>
+<nav class={"p-10 flex flex-row-reverse items-end " + ($page.url.pathname !== '/' ? 'bg-white' : '')}>
+    <button on:click={toggleNavbar} class="z-50 relative">
         <Menu style={"font-size: 1.7em; " + ($page.url.pathname === "/" || showNavbar ? "color: white;" : "color: black;")} />
     </button>
 </nav>
-{#if showNavbar === true}
-    <div class="navbar z-100 text-white fixed flex flex-col gap-y-3 px-9 h-screen w-screen">
-        {#if userId === ""}
-            <a href="/">Home</a>
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
-        {:else}
-            <a href="/" on:click={() => {toggleNavbar()}}>Home</a>
-            <a href="/app/dashboard" on:click={() => {toggleNavbar()}}>Dashboard</a>
-        {/if}        
-    </div>
-{:else}
-    <slot />
-{/if}
 
+<div class={`fixed overflow-hidden top-0 right-0 h-screen w-screen bg-[#DF793F] z-40 text-white flex flex-col gap-y-3 px-9 py-28 transition-transform duration-300 ease-in-out ${showNavbar ? 'translate-x-0' : 'translate-x-full'}`}>
+    {#if userId === ""}
+        <a href="/" on:click={toggleNavbar}>Home</a>
+        <a href="/login" on:click={toggleNavbar}>Login</a>
+        <a href="/register" on:click={toggleNavbar}>Register</a>
+    {:else}
+        <a href="/" on:click={toggleNavbar}>Home</a>
+        <a href="/app/dashboard" on:click={toggleNavbar}>Dashboard</a>
+    {/if}
+</div>
+
+<slot />
 
 <style>
     @font-face {
@@ -50,6 +47,14 @@
     @font-face {
         font-family: "EncodeMed";
         src: url("/fonts/EncodeSansExpanded-Medium.ttf") format("truetype");
+    }
+
+    .translate-x-full {
+        transform: translateX(100%);
+    }
+
+    .translate-x-0 {
+        transform: translateX(0%);
     }
 
     .navbar {
