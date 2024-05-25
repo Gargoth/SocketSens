95db162931c78cedc1cfd4cabe92707aa3d648cf
CJ Rosales <gargoth13@gmail.com>
2024-05-26 03:13:56 +0800

    fix(API): POST endpoint now returns response

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,15 +1,23 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
 
-export async function GET(event: RequestEvent) : Promise<Response> {
-  const sampleValue = {
-    name: 'Ceej',
-    description: 'Very pogi',
-    money: 0,
-  };
+export async function GET(event: RequestEvent): Promise<Response> {
+	const sampleValue = {
+		name: 'Ceej',
+		description: 'Very pogi',
+    message: 'Great job submitting a GET request!',
+		money: 0
+	};
+
 	return json(sampleValue);
 }
 
-export async function POST(event: RequestEvent) : Promise<Response> {
-  const request = event.request;
-	return await request.json();
+export async function POST(event: RequestEvent): Promise<Response> {
+	const sampleValue = {
+		name: 'Ceej',
+		description: 'Very pogi',
+    message: 'Great job submitting a POST request!',
+		money: 1
+	};
+
+	return json(sampleValue);
 }
