e22a4444d8262a460deb605d9b7e548c026d0da4
splasherzz <juliasabado30@gmail.com>
2024-05-26 22:38:21 +0800

    feat: add waiting state to socket toggle

--- src/components/socket.svelte
+++ src/components/socket.svelte
@@ -4,6 +4,7 @@
     export let socketNum;
     export let socketPwr;
     export let isSocketOn;
+    export let isSocketWaiting;
 
     $: isGreen = isSocketOn ? 'text-green-500' : ''
 
@@ -27,13 +28,17 @@
         Max Cap: 1 kWh
     </div>
     <div class='togglestate text-center text-base mt-2.5'>
-        <div class="z-0 flex w-full flex-col items-center justify-center gap-6">
-            <label class="z-0 relative inline-flex cursor-pointer items-center">
-              <input bind:checked={isSocketOn} on:change={()=>toggleSwitch()} id="switch" type="checkbox" class="peer sr-only" />
-              <label for="switch" class="hidden"></label>
-              <div class="z-0 peer h-6 w-11 rounded-full border bg-neutral-400 after:z-0 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-500 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
-            </label>
-        </div>
+        {#if isSocketWaiting == true}
+            <div class="animate-pulse">Waiting...</div>
+        {:else}
+            <div class="z-0 flex w-full flex-col items-center justify-center gap-6">
+                <label class="z-0 relative inline-flex cursor-pointer items-center">
+                <input bind:checked={isSocketOn} on:click={()=>toggleSwitch()} id="switch" type="checkbox" class="peer sr-only" />
+                <label for="switch" class="hidden"></label>
+                <div class="z-0 peer h-6 w-11 rounded-full border bg-neutral-400 after:z-0 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-500 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+                </label>
+            </div>
+        {/if}
     </div>
 </div>
 
--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -1,11 +1,19 @@
 <script>
 	import Socket from "../../../components/socket.svelte";
-  import { toggles } from "../../../stores/toggleStates";
+  import { toggles, isWaiting } from "../../../stores/toggleStates";
 
   function changeStates(index) {
     $toggles[index] = !$toggles[index]
   }
 
+  function changeWaitingStates(index) {
+    $isWaiting[index] = !$isWaiting[index]
+    // currently used setTimeout to simulate waiting for the socket to turn on/off (modify na lang once backend stuff is ok na)
+    setTimeout(() => {
+      changeStates(index)
+      $isWaiting[index] = !$isWaiting[index]
+    }, 2000)
+  }
 </script>
 
 <div class='block'>
@@ -14,10 +22,10 @@
   </h3>
 
   <div class="thingy grid grid-cols-2 gap-4 p-4 pt-8">
-    <Socket socketNum={1} socketPwr={0.15} isSocketOn={$toggles[0]} on:toggle={()=>changeStates(0)}/>
-    <Socket socketNum={2} socketPwr={0.35} isSocketOn={$toggles[1]} on:toggle={()=>changeStates(1)}/>
-    <Socket socketNum={3} socketPwr={0.1} isSocketOn={$toggles[2]} on:toggle={()=>changeStates(2)}/>
-    <Socket socketNum={4} socketPwr={0.5} isSocketOn={$toggles[3]} on:toggle={()=>changeStates(3)}/>
+    <Socket socketNum={1} socketPwr={0.15} isSocketOn={$toggles[0]} on:toggle={()=>changeWaitingStates(0)} isSocketWaiting={$isWaiting[0]}/>
+    <Socket socketNum={2} socketPwr={0.35} isSocketOn={$toggles[1]} on:toggle={()=>changeWaitingStates(1)} isSocketWaiting={$isWaiting[1]}/>
+    <Socket socketNum={3} socketPwr={0.1} isSocketOn={$toggles[2]} on:toggle={()=>changeWaitingStates(2)} isSocketWaiting={$isWaiting[2]}/>
+    <Socket socketNum={4} socketPwr={0.5} isSocketOn={$toggles[3]} on:toggle={()=>changeWaitingStates(3)} isSocketWaiting={$isWaiting[3]}/>
   </div>
 </div>
 
