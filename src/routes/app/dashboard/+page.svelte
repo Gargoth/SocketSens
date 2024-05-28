e1febb5c3dac74dd0c476b351fd55ff1dbea0f6a
splasherzz <juliasabado30@gmail.com>
2024-05-28 16:13:09 +0800

    feat: add energy threshold dropdown and threshold stores

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -2,6 +2,18 @@
 	import Socket from "../../../components/socket.svelte";
 	import { clientState } from "../../../stores/clientState";
   import { toggles, isWaiting } from "../../../stores/toggleStates";
+  import { softlimitThreshold } from "../../../stores/thresholdStore";
+  import { totalConsumption } from "../../../stores/totalConsumptionStore";
+  $: console.log($softlimitThreshold);
+  // let softThreshold = $softlimitThreshold;
+  
+  // function setSoftConsumption(value) {
+    //   softThreshold = value
+    //   $softlimitThreshold = value
+    //   console.log($softlimitThreshold)
+    // }
+    
+    let defaultThreshold = '1 kWh';
 
   function changeStates(index) {
     $toggles[index] = !$toggles[index];
@@ -37,20 +49,33 @@
     Dashboard
   </h3>
   <div class="bg-gradient-to-r from-orange-500/[.8] to-orange-700/[.8] rounded-2xl h-24 mx-4 p-3 px-4">
-    <span class="bg-white rounded-full px-2 py-1 text-orange-600 text-xs">Daily Power Usage</span>
-    <p class="text-3xl text-white mt-2">10.33 kWh</p>
+    <span class="bg-white rounded-full px-2 py-1 text-orange-600 text-xs">Total Energy Consumption</span>
+    <p class="text-3xl text-white mt-2">{$totalConsumption} kWh</p>
   </div>
 </div>
+
+<div class="mt-6">
+  <h3 class="mysocket pl-6 text-2xl mx-1">
+    Energy Threshold
+  </h3>
+  <div class="mx-14 mt-6">
+    <select bind:value={$softlimitThreshold} name="threshold" class="block appearance-none w-full text-center text-xl bg-gray-200 border border-gray-200 text-black py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+      <option value={0.2}>0.2 kWh</option>
+      <option value={0.35}>0.35 kWh</option>
+      <option value={1}>1 kWh</option>
+    </select>
+  </div>
+</div> 
 
 <div class="mt-6">
   <h3 class="mysocket pl-6 text-2xl mx-1">
     My Sockets
   </h3>
   <div class="thingy grid grid-cols-2 gap-4 p-4 pt-6">
-    <Socket socketNum={1} socketPwr={0.15} isSocketOn={$toggles[0]} on:toggle={()=>changeWaitingStates(0)} isSocketWaiting={$isWaiting[0]}/>
-    <Socket socketNum={2} socketPwr={0.35} isSocketOn={$toggles[1]} on:toggle={()=>changeWaitingStates(1)} isSocketWaiting={$isWaiting[1]}/>
-    <Socket socketNum={3} socketPwr={0.1} isSocketOn={$toggles[2]} on:toggle={()=>changeWaitingStates(2)} isSocketWaiting={$isWaiting[2]}/>
-    <Socket socketNum={4} socketPwr={0.5} isSocketOn={$toggles[3]} on:toggle={()=>changeWaitingStates(3)} isSocketWaiting={$isWaiting[3]}/>
+    <Socket socketNum={1} socketName={'Electric Fan'} socketPwr={0.15} isSocketOn={$toggles[0]} on:toggle={()=>changeWaitingStates(0)} isSocketWaiting={$isWaiting[0]}/>
+    <Socket socketNum={2} socketName={'Lamp'} socketPwr={0.35} isSocketOn={$toggles[1]} on:toggle={()=>changeWaitingStates(1)} isSocketWaiting={$isWaiting[1]}/>
+    <Socket socketNum={3} socketName={'Charger'} socketPwr={0.1} isSocketOn={$toggles[2]} on:toggle={()=>changeWaitingStates(2)} isSocketWaiting={$isWaiting[2]}/>
+    <Socket socketNum={4} socketName={'Hairdryer'} socketPwr={0.5} isSocketOn={$toggles[3]} on:toggle={()=>changeWaitingStates(3)} isSocketWaiting={$isWaiting[3]}/>
   </div>
 </div>
 
@@ -78,6 +103,10 @@
     @font-face {
         font-family: "EncodeMed";
         src: url("/fonts/EncodeSansExpanded-Medium.ttf") format("truetype");
+    }  
+
+    option, select {
+      font-family: "EncodeBold", sans-serif;
     }
 
     h3 {
