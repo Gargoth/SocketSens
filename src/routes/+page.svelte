de56b83ec3f2bcbf7d3346489c83788cf9038442
Shanic <drei.tiangco@gmail.com>
2024-05-30 04:29:07 +0800

    feat: add about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<script>
+<!-- <script>
 	import { testNotifications } from '$lib/notifications';
-</script>
+</script> -->
 
 <div class="text flex flex-col justify-center items-center text-center px-6">
 	<h1 class="fixed text-3xl top-1/2 mt-20 px-4">
@@ -13,9 +13,9 @@
 </div>
 
 <div class="fixed bottom-0 w-full mb-2 text-center p-6">
-	<button on:click={testNotifications} class="w-64 p-2 text-white rounded-full bg-orange-500">
+	<a href="/about" class="w-64 p-2.5 px-16 text-white rounded-full bg-orange-500">
 		Get Started
-	</button>
+	</a>
 </div>
 
 <style>
@@ -33,19 +33,9 @@
 		font-family: 'EncodeMed';
 		src: url('/fonts/EncodeSansExpanded-Medium.ttf') format('truetype');
 	}
-	h1 {
-		font-family: 'EncodeBold', sans-serif;
-	}
-
-	button {
-		font-family: 'EncodeBold', sans-serif;
-	}
 
 	:global(body) {
 		min-height: 100vh;
-	}
-
-	.name {
 		font-family: 'EncodeBold', sans-serif;
 	}
 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+    import video from '../../lib/sample-vid.mp4';
+
+    let showModal = false;
+
+    function openModal() {
+        showModal = true;
+    }
+
+    function closeModal() {
+        showModal = false;
+    }
+</script>
+
+<div class="w-full mb-2 text-center p-6">
+    <button class="w-64 p-2 text-white rounded-full bg-orange-500" on:click={openModal}>
+        Watch Demo
+    </button>
+</div>
+
+{#if showModal}
+    <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" on:click={closeModal}>
+        <div class="p-4 rounded-lg shadow-lg max-w-lg w-full" on:click|stopPropagation>
+            <span class="float-right text-gray-500 text-2xl" on:click={closeModal}>&times;</span>
+            <video controls class="w-full mt-4">
+                <source src={video} type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+{/if}
+
+
+<style>
+    @font-face {
+        font-family: 'EncodeBold';
+        src: url('/fonts/EncodeSansExpanded-Bold.ttf') format('truetype');
+    }
+
+    @font-face {
+        font-family: 'EncodeSB';
+        src: url('/fonts/EncodeSansExpanded-SemiBold.ttf') format('truetype');
+    }
+
+    @font-face {
+        font-family: 'EncodeMed';
+        src: url('/fonts/EncodeSansExpanded-Medium.ttf') format('truetype');
+    }
+
+    h1 {
+        font-family: 'EncodeBold', sans-serif;
+    }
+
+    button {
+        font-family: 'EncodeBold', sans-serif;
+    }
+
+    .name {
+        font-family: 'EncodeBold', sans-serif;
+    }
+
+    .sub {
+        font-family: 'EncodeMed', sans-serif;
+    }
+    .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: black;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    :global(body) {
+        min-height: 100vh;
+    }
+</style>
