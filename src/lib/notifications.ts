1d062e151f134fe3d98e2eb6305244211b9465d3
smtnhacker <themikosurara@gmail.com>
2024-05-29 10:52:18 +0800

    chore: change notif text

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -1,9 +1,31 @@
 export async function testNotifications() {
 	const result = await Notification.requestPermission();
 	if (result === 'granted') {
-		const noti = new Notification('Hello!', {
-			body: 'Ang pogi ni Ceej!'
+		const noti = new Notification('Notifications is Enabled!', {
+			body: 'Thanks for subscribing'
 		});
 		noti.onclick = () => alert('clicked');
+	}
+}
+
+export async function askNotifications() {
+	const result = await Notification.requestPermission();
+	if (result === 'granted') {
+		const noti = new Notification('Notifications is Enabled!', {
+			body: 'Thanks for subscribing'
+		});
+		noti.onclick = () => alert('clicked');
+	}
+}
+
+export async function NotifyWarning() {
+	try{
+		const noti = new Notification('Hello!', {
+			body: '2nd notif!'
+		});
+		noti.onclick = () => alert('clicked');
+
+	} catch (e) {
+		console.log(e)
 	}
 }
