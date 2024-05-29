d3660b6c3d94449d06dcb9348300ca278abe6653
Juls <112995476+splasherzz@users.noreply.github.com>
2024-05-29 19:32:39 +0800

    chore: try catch notifications (#10)

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -1,19 +1,27 @@
 export async function testNotifications() {
-	const result = await Notification.requestPermission();
-	if (result === 'granted') {
-		const registration = await navigator.serviceWorker.ready;
-		registration.showNotification('Notification', {
-			body: 'Thanks for using SocketSens!'
-		})
+	try {
+		const result = await Notification.requestPermission();
+		if (result === 'granted') {
+			const registration = await navigator.serviceWorker.ready;
+			registration.showNotification('Notification', {
+				body: 'Thanks for using SocketSens!'
+			})
+		}
+	} catch(e) {
+		console.error(e)
 	}
 }
 
 export async function notifyWarning(value) {
-	const result = await Notification.requestPermission();
-	if (result === 'granted') {
-		const registration = await navigator.serviceWorker.ready;
-		registration.showNotification('Soft Limit Breached', {
-			body: `Warning! SocketSens detected that you have consumed ${value}kWh`
-		})
+	try{
+		const result = await Notification.requestPermission();
+		if (result === 'granted') {
+			const registration = await navigator.serviceWorker.ready;
+			registration.showNotification('Soft Limit Breached', {
+				body: `Warning! SocketSens detected that you have consumed ${value}kWh`
+			})
+		}
+	} catch(e) {
+		console.error(e)
 	}
 }
