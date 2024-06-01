045766b0471af61c35db445e8474b003d74e1e9f
francecalma <113011006+francecalma@users.noreply.github.com>
2024-06-01 11:36:04 +0800

    error: switch to scatter plot

--- src/components/energygraph.svelte
+++ src/components/energygraph.svelte
@@ -14,18 +14,19 @@
 	async function computeTotalEnergy() {
 		const { data } = await getAllElecRowsToday();
 		const elecRows = data as ElecRow[];
-		const cumulativeEnergy: { time: string; totalEnergy: number }[] = [];
-		const cTime : string[] = [];
-		const cEnergy : string[] = [];
+		const cumulativeEnergy: { x: number, y: number }[] = [];
+		const x : number[] = [];
+		const y : number[] = [];
 		if (elecRows) {
+			// cumulativeEnergy.push({elecRows[0].time.getTime(), elecRows[0].energy});
 			cumulativeEnergy.push({
-				time: elecRows[0].time.toLocaleString().substring(11, 16),
-				totalEnergy: elecRows[0].energy
+				x: elecRows[0].time.getTime(),
+				y: elecRows[0].energy
 			});
-			cTime.push(elecRows[0].time.toLocaleString().substring(11, 16));
-			cEnergy.push(elecRows[0].energy.toString());
+			x.push(elecRows[0].time.getTime());
+			y.push(elecRows[0].energy);
 			for (let i = 1; i < elecRows.length; i++) {
-				let convertDate = elecRows[i].time.toLocaleString().substring(11, 16);
+				let convertDate = elecRows[i].time.getTime();
 				// energy > 0 AND energy increased
 				if (elecRows[i].energy > 0 && elecRows[i].energy > elecRows[i - 1].energy) {
 					var prev = elecRows[i - 1].energy;
@@ -33,19 +34,19 @@
 						prev = 0;
 					}
 					cumulativeEnergy.push({
-						time: convertDate,
-						totalEnergy:
-							cumulativeEnergy[i - 1].totalEnergy + elecRows[i].energy - prev
+						x: convertDate,
+						y:
+							cumulativeEnergy[i - 1].y + elecRows[i].energy - prev
 					});
-					cTime.push(convertDate);
-					cEnergy.push((cumulativeEnergy[i - 1].totalEnergy + elecRows[i].energy - prev).toString());
+					x.push(convertDate);
+					y.push((cumulativeEnergy[i - 1].y + elecRows[i].energy - prev));
 				} else {
 					cumulativeEnergy.push({
-						time: convertDate,
-						totalEnergy: cumulativeEnergy[i - 1].totalEnergy
+						x: convertDate,
+						y: cumulativeEnergy[i - 1].y
 					});
-					cTime.push(convertDate);
-					cEnergy.push((cumulativeEnergy[i - 1].totalEnergy).toString());
+					x.push(convertDate);
+					y.push((cumulativeEnergy[i - 1].y));
 				}
 				// console.log(cumulativeEnergy[i].time);
 			}
@@ -53,7 +54,7 @@
 		// console.log(cumulativeEnergy.time);
 		$totalConsumption =
 			cumulativeEnergy.length > 0 ? cumulativeEnergy[cumulativeEnergy.length - 1].totalEnergy : 0;
-		return {cumulativeEnergy, cTime, cEnergy};
+		return {cumulativeEnergy, x, y};
 	}
   //
 	// Subscribe to elec changes
@@ -72,34 +73,35 @@
 	// let data = computeTotalEnergy();
 	let ctx = 'chartData';
 	onMount( async () => {
-		let {cumulativeEnergy, cTime, cEnergy} = await computeTotalEnergy();
+		let {cumulativeEnergy, x, y} = await computeTotalEnergy();
 		console.log(cumulativeEnergy);
 		// if (!cEnergy) {
 		// 	return null;
 		// }
-		var minE = cEnergy[0];
-		var maxE = cEnergy[0];
-		for (var a of cEnergy) {
+		var minE = y[0];
+		var maxE = y[0];
+		for (var a of y) {
 			if (a < minE) minE = a;
 			if (a > maxE) maxE = a;
 		}
 
 		var chartData = new Chart(ctx, {
-			type: 'line',
-			data: {
-				labels: cTime,
-				datasets: [{
-					label: 'Total Energy',
-					data: cEnergy,
-					// borderWidth: 0.3,
-					// tension: 0.3,
-					pointRadius: 0, 
-					// pointBackgroundColor: 'blue',
-					fill: false,
-					borderColor: 'white'
-					// backgroundColor: 'orange'
-				}]
-			},
+			type: 'scatter',
+			data: cumulativeEnergy,
+			// {
+			// 	labels: cTime,
+			// 	datasets: [{
+			// 		label: 'Total Energy',
+			// 		data: cEnergy,
+			// 		// borderWidth: 0.3,
+			// 		// tension: 0.3,
+			// 		pointRadius: 0, 
+			// 		// pointBackgroundColor: 'blue',
+			// 		fill: false,
+			// 		borderColor: 'white'
+			// 		// backgroundColor: 'orange'
+			// 	}]
+			// },
 			options: {
 				scales: {
 					x: {
